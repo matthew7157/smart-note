c7b7c38a1cfcf345c06225bf4faa0b7527164e51
matthew7157 <matthew71757@gmail.com>
2026-04-15 20:04:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+const CACHE = 'smartnote-v1';
+  const FILES = [
+    '/smart-note/',
+    '/smart-note/index.html',
+    '/smart-note/manifest.json',
+    '/smart-note/icon.png'
+  ];
 
+  self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));      
+  });
+
+  self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request))       
+    );
+  });
